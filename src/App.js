5eb4317d7aaccd9e689af50ec87e256c19f44e5f
David <davidphang431@gmail.com>
2022-01-10 09:41:42 +0700

    Install tailwind and axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <h1 className="text-3xl font-bold font-mono">Hello world!</h1>;
 }
 
 export default App;
