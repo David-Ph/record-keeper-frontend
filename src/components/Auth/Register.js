898d4bfac2a2e28575cace0ef783311caad66353
David <davidphang431@gmail.com>
2022-01-11 09:27:56 +0700

    Add validator function

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useRef, Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import Textbox from "../UI/Textbox/Textbox";
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
 function Register() {
-  return <div>
-    <h1>Register Page</h1>
-  </div>;
+  const usernameInputRef = useRef();
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
+
+  const registerHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Fragment>
+      <Textbox>
+        <h1 className="font-semibold text-2xl">Register</h1>
+      </Textbox>
+      <Textbox>
+        <section>
+          <form onSubmit={registerHandler}>
+            <Input
+              label="Username:"
+              ref={usernameInputRef}
+              input={{
+                id: "username",
+                type: "text",
+                minlength: "3",
+                maxLength: "25",
+              }}
+            />
+            <Input
+              label="Email:"
+              ref={emailInputRef}
+              input={{
+                id: "email",
+                type: "email",
+                minlength: "1",
+              }}
+            />
+            <Input
+              label="Password:"
+              ref={passwordInputRef}
+              input={{
+                id: "password",
+                type: "password",
+                minlength: "6",
+              }}
+            />
+            <Input
+              label="Confirm Password:"
+              ref={confirmPasswordInputRef}
+              input={{
+                id: "confirmPassword",
+                type: "password",
+                minlength: "6",
+              }}
+            />
+            <Button type="submit">Register</Button>
+          </form>
+        </section>
+        <p className="text-xs mt-2 text-right">
+          <Link style={{ color: "blue" }} to="/login">
+            Click here
+          </Link>{" "}
+          to sign in!
+        </p>
+      </Textbox>
+    </Fragment>
+  );
 }
 
 export default Register;
---
+++ src/helper/validators/AuthValidator.js
@@ -0,0 +1,36 @@
+import validator from "validator";
+
+export const usernameValidator = (username = "") => {
+  const isValid =
+    validator.isAlphanumeric(username, "en-US", { ignore: "_-" }) &&
+    validator.isLength(username, { min: 3, max: 25 });
+  const message = isValid
+    ? "OK"
+    : "Username has to be between 3 to 25 characters and can only contain alphanumeric characters, '-' and '_'";
+  return { isValid, message };
+};
+
+export const emailValidator = (email = "") => {
+  const isValid = validator.isEmail(email.toString());
+  const message = isValid ? "OK" : "Invalid Email";
+  return { isValid, message };
+};
+
+export const passwordValidator = (password = "") => {
+  const isValid = validator.isStrongPassword(password.toString(), {
+    minLength: 6,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 0,
+    minSymbols: 0,
+  });
+
+  const message = isValid
+    ? "OK"
+    : "Password needs to have at least 6 characters, 1 uppercase, and 1 lowercase";
+  return { isValid, message };
+};
+
+export const confirmPasswordValidator = (password, confirmPassword) => {
+  return password === confirmPassword;
+};
