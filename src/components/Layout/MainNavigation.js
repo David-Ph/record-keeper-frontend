40044535d5689d5577c053939502ceeaf74537e6
David <davidphang431@gmail.com>
2022-01-19 17:34:31 +0700

    Remove campaign options from header

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -9,9 +9,6 @@ function MainNavigation() {
       <div className="flex flex-col items-center justify-between bg-primary p-2 rounded-md md:flex-row border-2 border-black">
         <Logo />
         <div className="md:-order-last py-2 text-lg">Saltshakers</div>
-        <div className="md:-order-last text-sm">
-          <CampaignOptions />
-        </div>
       </div>
     </header>
   );
